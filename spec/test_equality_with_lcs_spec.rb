d880d16bef5976b233663482b22f4d1e89dea0a3
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-11-07 23:45:00 -0800

    Fixed tests after moving spec code into it's own files.

--- spec/test_equality_with_lcs_spec.rb
+++ spec/test_equality_with_lcs_spec.rb
@@ -1,7 +1,45 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TestEqualityWithLcs" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "passes" do
+    'actual'.should be_equal_with_lcs('actual')
+  end
+
+  it "doesn't pass" do
+    'actual'.should_not be_equal_with_lcs('something else so this test will fail and we can test the resulting string')
+  end
+
+  it "get's correct output" do
+    self.class.colorize = false
+    # careful of the whitespace required here.
+    expected_error = "---------------------------------- ---------- --- ----------- + -----------
+                                  a          c   t           ual           
+something else so this test will fail and we can test the resu lting string"
+    lambda{ 
+      'actual'.should be_equal_with_lcs('something else so this test will fail and we can test the resulting string')
+    }.should raise_error(Spec::Expectations::ExpectationNotMetError, expected_error)
+  end
+
+  it "get's correct output with multi-line string comparison" do
+    self.class.colorize = false
+    # careful of the whitespace required here.
+    seq1 = "actual
+ly
+I
+didn't want it all
+on one
+line"
+    seq2 = "actually I did want it on ...
+well a couple lines."
+
+    expected_error = "      -----------------------
+actual                       
+actually I did want it on ...
+-- !----------------
+  ly                
+well a couple lines."
+    lambda{ 
+      seq1.should be_equal_with_lcs(seq2)
+    }.should raise_error(Spec::Expectations::ExpectationNotMetError, expected_error)
   end
 end
